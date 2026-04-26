d36be28c84dc103e6d9a6adcf553b80ffe414fca
Dartoke <david.moisi@yahoo.com>
2026-04-26 21:44:18 +0300

    add inline to function

--- proiect/utile.h
+++ proiect/utile.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstring>
 
-char* copiazaSir(const char* sursa) {
+inline char* copiazaSir(const char* sursa) {
     if (!sursa) return nullptr;
     char* dest = new char[strlen(sursa) + 1];
     strcpy(dest, sursa);
